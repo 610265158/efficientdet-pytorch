a8d699eb6cb00a3d0b896c016e7c2621ff19758e
lz <2120140200@mail.nankai.edu.cn>
2020-07-28 01:27:11 +0800

    safe

--- lib/dataset/dataietr.py
+++ lib/dataset/dataietr.py
@@ -220,10 +220,10 @@ class DsfdDataIter():
         self.no_crop_transform=A.Compose(
                                 [
                                     A.OneOf([
-                                        A.HueSaturationValue(hue_shift_limit=20, sat_shift_limit=20,
-                                                             val_shift_limit=20, p=1.),
-                                        A.RandomBrightnessContrast(brightness_limit=0.5,
-                                                                   contrast_limit=0.5, p=1.),
+                                        A.HueSaturationValue(hue_shift_limit=0.2, sat_shift_limit=0.2,
+                                                             val_shift_limit=0.2, p=1.),
+                                        A.RandomBrightnessContrast(brightness_limit=0.2,
+                                                                   contrast_limit=0.2, p=1.),
                                     ],p=0.9),
                                     # A.ToGray(p=0.01),
                                     #A.Cutout(num_holes=8, max_h_size=64, max_w_size=64, fill_value=0, p=0.5),
@@ -234,10 +234,10 @@ class DsfdDataIter():
                                 [
                                     A.RandomSizedCrop(min_max_height=(800, 800), height=1024, width=1024, p=0.5),
                                     A.OneOf([
-                                        A.HueSaturationValue(hue_shift_limit=20, sat_shift_limit= 20,
-                                                             val_shift_limit=20, p=1.),
-                                        A.RandomBrightnessContrast(brightness_limit=0.5,
-                                                                   contrast_limit=0.5, p=1.),
+                                        A.HueSaturationValue(hue_shift_limit=0.2, sat_shift_limit= 0.2,
+                                                             val_shift_limit=0.2, p=1.),
+                                        A.RandomBrightnessContrast(brightness_limit=0.2,
+                                                                   contrast_limit=0.2, p=1.),
                                     ],p=0.9),
                                     # A.ToGray(p=0.01),
                                     A.HorizontalFlip(p=0.5),
