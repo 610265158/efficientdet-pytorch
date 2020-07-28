bbe82f63bd9e797af1e7f1a28ff01856d297550f
liangzi <liangzi@weibo.staff.com>
2020-07-28 17:36:40 +0800

    add rgb shift

--- lib/dataset/dataietr.py
+++ lib/dataset/dataietr.py
@@ -239,10 +239,14 @@ class DsfdDataIter():
         self.no_crop_transform=A.Compose(
                                 [
                                     A.OneOf([
-                                        A.HueSaturationValue( p=0.9),
+                                        A.HueSaturationValue(hue_shift_limit=5,
+                                                             sat_shift_limit=5,
+                                                             val_shift_limit=5,
+                                                             p=0.9),
                                         A.RandomBrightnessContrast(brightness_limit=0.3,
                                                                    contrast_limit=0.3, p=0.9),
                                     ],p=0.9),
+                                    A.RGBShift(p=0.9)
                                     # A.ToGray(p=0.01),
                                     #A.Cutout(num_holes=8, max_h_size=64, max_w_size=64, fill_value=0, p=0.5),
                                 ],
@@ -256,10 +260,14 @@ class DsfdDataIter():
                                     A.VerticalFlip(p=0.5),
                                     A.Resize(height=cfg.DATA.hin, width=cfg.DATA.win, p=1),
                                     A.OneOf([
-                                        A.HueSaturationValue(p=0.9),
+                                        A.HueSaturationValue(hue_shift_limit=5,
+                                                             sat_shift_limit=5,
+                                                             val_shift_limit=5,
+                                                             p=0.9),
                                         A.RandomBrightnessContrast(brightness_limit=0.2,
                                                                    contrast_limit=0.2, p=0.9),
                                     ], p=0.9),
+                                    A.RGBShift(p=0.9)
                                     #A.Cutout(num_holes=8, max_h_size=64, max_w_size=64, fill_value=0, p=0.5),
 
                                 ],
